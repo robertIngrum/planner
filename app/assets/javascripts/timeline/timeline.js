f525c16ee28e3ac7d087010d56cae142ccdeab2a
Robert Ingrum <rkingrum@gmail.com>
2016-10-29 14:43:44 -0700

    Displaying todos

--- app/assets/javascripts/timeline/timeline.js
+++ app/assets/javascripts/timeline/timeline.js
@@ -3,7 +3,9 @@ var Timeline = function(id) {
   this.name = null;
   this.start = null;
   this.end = null;
-  this.items = { tasks: [], events: [], todo: [] };
+  this.items = { tasks: [], events: [], todos: [] };
+
+  this.timeline_item_container = $('#timeline_items');
 
   this.load();
   this.show();
@@ -13,17 +15,52 @@ Timeline.prototype.load = function() {
   this.name = 'Test 1';
   this.start = false;
   this.end = false;
-  this.items.todo.push({
+  this.items.todos.push({
     name: 'Awesome thing to do',
     description: 'Do this thing and it has this priority because it\'s a todo.',
     created: 0,
     archived: false,
     start: 0,
     end: false,
-    priority: '.7'
+    priority: '.0'
+  });
+  this.items.todos.push({
+    name: 'Awesome thing to do number 2',
+    description: 'Do this thing and it has this priority because it\'s a todo.',
+    created: 0,
+    archived: false,
+    start: 0,
+    end: false,
+    priority: '.0'
   });
 };
 
 Timeline.prototype.show = function() {
+  this.show_tasks();
+  this.show_events();
+  this.show_todos();
+};
 
+Timeline.prototype.show_tasks = function() {
+  $.each(this.items.tasks, function(key, val) {});
+};
+
+Timeline.prototype.show_events = function() {
+  $.each(this.items.events, function(key, val) {});
+};
+
+Timeline.prototype.show_todos = function() {
+  var self = this;
+
+  $.each(this.items.todos, function(_, data) {
+    var todo = $('<div>')
+        .html(data.name)
+        .addClass('todo')
+        .attr('data-priority', data.priority)
+        .css({
+          width: '500px'
+        });
+
+    self.timeline_item_container.append(todo);
+  });
 };
